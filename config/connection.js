22e7e9fffc8a0cb55d66c6c3a2b4795d892383d3
Samuel <Samuelpmontague@gmail.com>
2021-07-20 21:27:28 -0500

    attempting to fix bugs

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ const sequelize = process.env.JAWSDB_URL
       dialectOptions: {
         decimalNumbers: true,
       },
-      port: 3306
+      port: 3036
     });
 
 module.exports = sequelize;
